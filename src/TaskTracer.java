abdc470c9d7b5bde518c89938c6089a2f83c98c2
KnTui <abdoulayesidie10@gmail.com>
2026-03-07 00:29:50 +0100

    The entire code was basically rewritten. Introducing three variable as {prefix, command, commandRequest}. Also the verification are removed for now. This is the new starting point.

--- src/TaskTracer.java
+++ src/TaskTracer.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class TaskTracer{
     public static void main(String[] args){
+        /*
+         * Task tracker is a project used to track and manage your tasks. In this task, you will build a simple command
+         * line interface (CLI) to track what you need to do, what you have done, and what you are currently working on.
+         * This project will help you practice your programming skills, including working with the filesystem, handling
+         * user inputs, and building a simple CLI application.
+         */
+
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Welcome to Task-Tracer!");
+        System.out.println("Enter your command down below");
         System.out.print("> ");
-        String userInput = input.nextLine();
 
-        while(!userInput.startsWith("task-cli")){
-            if(userInput.length() < 8){
-                System.out.println("Too short");
-            }else{
-                System.out.println(userInput.substring(0, 7) + "is not recognized as a command!");
-            }
+        String prefix = input.next(); // Prefix of my script, it should be 'task-cli'
+        String command = input.next(); // Add, Delete, Update and more
+        String commandRequest = input.next(); // It depends on the command, but it can be something like this 'task-cli add HomeWork'
 
-            System.out.println("Make sur to enter 'task-cli' before your request.");
-            System.out.print("> ");
-            userInput = input.nextLine();
-        }
-        System.out.println(userInput);
+        System.out.println(prefix + " " + command + " " + commandRequest);
     }
 }
